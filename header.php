6e3a47f04315d61ae1aae05874a447496d0ef2d5
Jonty Morris <jonty.morris@yahoo.co.nz>
2021-06-26 17:07:05 +1200

    Custom hoome header

--- header.php
+++ header.php
@@ -1,8 +1,21 @@
-<header>
-	<div class="container">
-		<a href="/">
-			<img class="logo" src="<?php echo get_template_directory_uri() ?>/images/logo.png">
-		</a>
-		<span class="motto">Your friendly experts in WordPress security</span>
+<?php if (is_front_page()) : ?>
+
+	<!-- home page banner -->
+	<div class="home-header" style="background-image: url(<?php echo get_template_directory_uri() ?>/images/banner.jpg)">
+		<img src="<?php echo get_template_directory_uri() ?>/images/logo-clear.png">
+		<span>Your friendly experts in WordPress security</span>
 	</div>
-</header>
+
+<?php else : ?>
+
+	<!-- general page banner -->
+	<header>
+		<div class="container">
+			<a href="/">
+				<img class="logo" src="<?php echo get_template_directory_uri() ?>/images/logo.png">
+			</a>
+			<span class="motto">Your friendly experts in WordPress security</span>
+		</div>
+	</header>
+
+<?php endif; ?>

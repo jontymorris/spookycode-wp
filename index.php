b6ea0bc73b52039f53bf0e95c44af218d5771b13
Jonty Morris <jonty.morris@yahoo.co.nz>
2021-06-29 20:36:02 +1200

    Added Google Analytics

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"> 
 		<?php wp_head(); ?>
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109546240-2"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-109546240-2');
+		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
